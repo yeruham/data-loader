517eea165608f7dd0fcf1c2434e18f1e66428be1
YeruhamMendelson <y0583207549@gmail.com>
2025-08-14 12:01:27 +0300

    enve variables in main.py to connect to mysql

--- services/main.py
+++ services/main.py
@@ -1,29 +1,41 @@
 from fastapi import FastAPI
 import uvicorn
 from DAL.DAL_DB import DAL
+import os
 
 
 app = FastAPI()
 
-host = ''
-user = ''
-password = ''
-database = ''
-table = 'data'
+
+app.state.db_host = os.getenv("DB_HOST", "localhost")
+app.state.db_user = os.getenv("DB_USER", "")
+app.state.db_password = os.getenv("DB_PASSWORD", "")
+app.state.db_database = os.getenv("DB_DATABASE", "db_data")
+app.state.db_table = os.getenv("DB_TABLE", "data")
+
+
+app.get('/')
+async def root():
+    return {"message": "this API of data table with people full-names,"
+            "by /data path"}
+
+
+app.get('/data')
+async def get_data():
+    data = select_data(host= app.state.db_host,
+                       user= app.state.db_user,
+                       password= app.state.db_password,
+                       database= app.state.db_database,
+                       table= app.state.db_table)
+    return data
+
+
 
 def select_data(host, user, password, database, table):
     DAL_data = DAL(host= host, user= user, password= password,
                    database= database, table= table)
     data = DAL_data.select()
     return data
-
-
-app.get('/data')
-async def root():
-    data = select_data(host= host, user= user, password= password,
-                   database= database, table= table)
-    return data
-
 
 
 if __name__ == "__main__":
