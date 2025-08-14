5ec7635d4b4510ff5043330e7f85c16facd6e8e3
YeruhamMendelson <y0583207549@gmail.com>
2025-08-14 16:35:46 +0300

    first commends tp openShift

---
+++ scripts/commands.bat
@@ -0,0 +1,22 @@
+# create mysql app
+oc new-app mysql-ephemeral --name mysql
+           (-e MYSQL_HOST=mysql
+           -e MYSQL_DATABASE=db_data -e MYSQL_USER=yeruham -e MYSQL_PASSWORD=123456)
+
+-- oc new-app mysql-persistent -p MYSQL_USER=yeruham
+-- -p MYSQL_PASSWORD=12345 -p MYSQL_DATABASE=db_data -p MYSQL_ROOT_PASSWORD=R12345
+
+# name of service
+oc get service
+
+# copy sql files to mysql-app
+oc cp ./create_data.sql name_pod:/tmp/create_data.sql
+oc cp ./insert_data.sql name_pod:/tmp/insert_data.sql
+# exec to pod for run sql files
+oc exec -it mysql-1-lhbbv -- bash
+
+# route by name of service
+oc expose service mysql
+
+
+oc new-app https://github.com/yeruham/data-loader --name data-loader
